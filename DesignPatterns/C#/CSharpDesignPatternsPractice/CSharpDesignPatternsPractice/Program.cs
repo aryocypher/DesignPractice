e7d59b9aab8c76626a2cd8fbc80d1454406c267d
aryocypher <aryocypher@gmail.com>
2024-07-10 00:05:23 +0530

    adapter pattern c#

--- DesignPatterns/C#/CSharpDesignPatternsPractice/CSharpDesignPatternsPractice/Program.cs
+++ DesignPatterns/C#/CSharpDesignPatternsPractice/CSharpDesignPatternsPractice/Program.cs
@@ -1,2 +1,27 @@
-﻿//// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿////// See https://aka.ms/new-console-template for more information
+////Console.WriteLine("Hello, World!");
+
+
+
+
+//int[] nums = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];   
+//bool IsEven(int n) => n % 2 == 0;
+
+//var res=nums.Where(x=>IsEven(2)).ToList();
+
+//foreach(int n in res)
+//{
+//    Console.WriteLine(n);
+//}
+
+
+//public class Test
+//{
+//    public int val { get; set; }
+//}
+
+//public struct InTest
+//{
+//    public string val { get; set; }
+//    public Test Test { get; set; }
+//}
---
+++ DesignPatterns/C#/CSharpDesignPatternsPractice/CSharpDesignPatternsPractice/Structural/Adapter/Adapter.cs
@@ -0,0 +1,20 @@
+﻿    public class Adaptee
+    {
+    public void PrintString(String s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+    public class Adapter
+    {
+    public readonly Adaptee adaptee;
+    public Adapter()
+    {
+        Adaptee adaptee = new Adaptee();
+    }
+
+    public void AdapterPrintString(String s)
+    {
+        Console.WriteLine(s);
+    }
+}
