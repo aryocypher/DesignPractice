6969bb01bfdae0698fec0cc67fa7e4dc33dd02dd
aryocypher <aryocypher@gmail.com>
2024-07-25 00:34:42 +0530

    Command design pattern

---
+++ DesignPatterns/C#/CSharpDesignPatternsPractice/CSharpDesignPatternsPractice/Design Patterns/Behavioural/Command/Command.cs
@@ -0,0 +1,110 @@
+﻿//Command is a behavioral design pattern that turns a request into a stand-alone object
+//    that contains all information about the request. This transformation lets you pass 
+//    requests as a method arguments, delay or queue a request’s execution, and support undoable operations.
+
+//TV tv = new TV();
+//Stereo stereo = new Stereo();
+
+//// Create command objects
+//ICommand turnOnTVCommand = new TurnOnCommand(tv);
+//ICommand turnOffTVCommand = new TurnOffCommand(tv);
+//ICommand adjustVolumeStereoCommand = new AdjustVolumeCommand(stereo);
+//ICommand changeChannelTVCommand = new ChangeChannelCommand(tv);
+
+//// Create remote control
+//RemoteControl remote = new RemoteControl();
+
+//// Set and execute commands
+//remote.setCommand(turnOnTVCommand);
+//remote.pressButton(); // Outputs: TV is now on
+
+//remote.setCommand(adjustVolumeStereoCommand);
+//remote.pressButton(); // Outputs: Volume adjusted
+
+//remote.setCommand(changeChannelTVCommand);
+//remote.pressButton(); // Outputs: Channel changed
+
+//remote.setCommand(turnOffTVCommand);
+//remote.pressButton(); // Outputs: TV is now off
+
+
+class RemoteControl
+{
+    private ICommand command;
+
+    public void setCommand(ICommand command)
+    {
+        this.command = command;
+    }
+
+    public void pressButton()
+    {
+        command.execute();
+    }
+}
+public interface ICommand
+{
+    void execute();
+}
+
+public class TurnOnCommand : ICommand
+{
+    private IDevice device;
+
+    public TurnOnCommand(IDevice device)
+    {
+        this.device = device;
+    }
+
+    public void execute()
+    {
+        device.turnOn();
+    }
+}
+
+// Concrete command for turning a device off
+public class TurnOffCommand : ICommand
+{
+    private IDevice device;
+
+    public TurnOffCommand(IDevice device)
+    {
+        this.device = device;
+    }
+
+    public void execute()
+    {
+        device.turnOff();
+    }
+}
+
+// Concrete command for adjusting the volume of a stereo
+public class AdjustVolumeCommand : ICommand
+{
+    private Stereo stereo;
+
+    public AdjustVolumeCommand(Stereo stereo)
+    {
+        this.stereo = stereo;
+    }
+    public void execute()
+    {
+        stereo.adjustVolume();
+    }
+}
+
+// Concrete command for changing the channel of a TV
+public class ChangeChannelCommand : ICommand
+{
+    private TV tv;
+
+    public ChangeChannelCommand(TV tv)
+    {
+        this.tv = tv;
+    }
+
+    public void execute()
+    {
+        tv.changeChannel();
+    }
+}
---
+++ DesignPatterns/C#/CSharpDesignPatternsPractice/CSharpDesignPatternsPractice/Design Patterns/Behavioural/Command/Devices.cs
@@ -0,0 +1,46 @@
+﻿
+
+// Receiver interface
+public interface IDevice
+{
+    void turnOn();
+    void turnOff();
+}
+
+// Concrete receiver for a TV
+public class TV : IDevice
+{
+    public void turnOn()
+    {
+        Console.WriteLine("TV is now on");
+    }
+
+    public void turnOff()
+    {
+        Console.WriteLine("TV is now off");
+    }
+
+    public void changeChannel()
+    {
+        Console.WriteLine("Channel changed");
+    }
+}
+
+// Concrete receiver for a stereo
+public class Stereo : IDevice
+{
+    public void turnOn()
+    {
+        Console.WriteLine("Stereo is now on");
+    }
+
+    public void turnOff()
+    {
+        Console.WriteLine("Stereo is now off");
+    }
+
+    public void adjustVolume()
+    {
+        Console.WriteLine("Volume adjusted");
+    }
+}
